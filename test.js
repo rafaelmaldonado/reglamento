e33b1ec7d1deb6ff0d18d2ef1ff8c53926a83b20
Rafael Maldonado <jr.maldonadoh@gmail.com>
2015-08-26 23:54:23 -0500

    delete unnecesary files, done testing for remove duplicated words

--- test.js
+++ test.js
@@ -1,40 +1,25 @@
-var a = new Array();
-a = [1,2,3,2,5,4,4,8,9,2];
+var a = [1,2,3,2,5,4,4,8,9,2];
 var b = [];
-console.log(a);
-var i = 0, j = 0, salir = false;
-while(i < a.length){
-	while(j < a.length){
-		if (i != j){
-			if (a[i] == a[j]){
-				console.log('. a[' + i + '] = ' + a[i] + ' a[' + j + '] = ' + a[j]);
-			} else {
-				b.push(a[i]);
-				salir = true;
-				console.log(b);
-				console.log('+ a[' + i + '] = ' + a[i] + ' a[' + j + '] = ' + a[j]);
-			}
-		} else {
-			if (a[i] == a[j]){
-				console.log('. a[' + i + '] = ' + a[i] + ' a[' + j + '] = ' + a[j]);
-			} else {
-				b.push(a[i]);
-				salir = true;
-				console.log(b);
-				console.log('+ a[' + i + '] = ' + a[i] + ' a[' + j + '] = ' + a[j]);
-			}
-		}
-		if (salir)
-			break;
-		j++;
-	}
-	i++;
-}
-console.log(b);
+var is_new = false;
 
-// Array Remove - By John Resig (MIT Licensed)
-Array.prototype.remove = function(from, to) {
-  var rest = this.slice((to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
-};
+for (var i in a){
+	for (var j in a)
+		if (i != j)
+			if (a[i] == a[j]){
+				is_new = false;
+				break;
+			}
+			else
+				is_new = true;
+		else
+			if (a[i] == a[j]){
+				is_new = true;
+				break;
+			}
+			else
+				is_new = false;
+	if (is_new)
+		b.push(a[i]);
+}
+
+console.log(b);
