5b3edcb3f1320879723371f6b5ed0729512698cf
Rafael Maldonado <juan.maldonado@televisatim.com>
2015-09-02 20:23:17 -0500

    remove spaces and logs

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,4 +1,3 @@
 module.exports = function (req, res){
-	console.log(req.body.search);
 	res.send(req.body.search);
 }
--- app/expressServer.js
+++ app/expressServer.js
@@ -6,11 +6,9 @@ var list = require('./controllers/list');
 var article = require('./controllers/article');
 var search = require('./controllers/search');
 
-var ExpressServer = function(config){
+module.exports =  ExpressServer = function(config){
     config = config || {};
-
     this.expressServer = express();
-
     this.expressServer.engine('html', swig.renderFile);
     this.expressServer.set('view engine', 'html');
     this.expressServer.set('views', __dirname + '/views');
@@ -19,20 +17,15 @@ var ExpressServer = function(config){
         extended: true
     }));
     swig.setDefaults({varControls:['[[',']]']});
-
     if(config.mode == 'development'){
         console.log('no cache');
         this.expressServer.set('view cache', false);
         swig.setDefaults({cache: false, varControls:['[[',']]']});
     }
-
     this.expressServer.get('/', function (req, res){
         home(req, res);
     });
-
     this.expressServer.post('/search', function (req, res){
         search(req, res);
     });
 };
-
-module.exports = ExpressServer;
