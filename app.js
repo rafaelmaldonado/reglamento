54f04e5ea66c75f659d5a1cc042c912dbb262517
Rafael Maldonado <juan.maldonado@televisatim.com>
2015-09-03 17:26:14 -0500

    showing results on the search

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ var app = new expressServer();
 
 var server = http.createServer(app.expressServer);
 server.listen(config.port, function(){
-	console.log('Express server running on port ' + config.port);
+	console.log('Regulation searcher running on ' + config.port);
 });
--- app/controllers/search.js
+++ app/controllers/search.js
@@ -5,7 +5,7 @@ module.exports = function (req, res){
 	words = (req.body.search).split(' ');
 	model(words, function(callback){
 		if (callback.length == 0)
-			res.render('../views/list', {list: 'No hay resultados para tu búsqueda'});
+			res.render('../views/list', {empty: 'No hay resultados para tu búsqueda'});
 		else
 			res.render('../views/list', {list: callback});
 	});
