86d3830a8117fe7a56e7de15ea730a7a356f3418
Rafael Maldonado <juan.maldonado@televisatim.com>
2015-09-04 17:29:01 -0500

    now the results are filtered

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -4,14 +4,39 @@ module.exports = function (req, res){
 	words = (req.query.q).split(' ');
 	model(words, function(callback){
 		for (var element in callback){
-			filtered = (callback[element].text).split(' ');
-			for (var i in words)
+			filtered = (callback[element].text).split(/[\r|\n|\s]/);
+			var resume = [];
+			for (var i in words){
 				for (var j in filtered){
-					var re = new RegExp(words[i], 'i');
-					if(re.test(normalize(filtered[j])))
+					var re = new RegExp(normalize(words[i]), 'i');
+					if(re.test(normalize(filtered[j]))){
+						
+						var init = 0, end = 0;
+						var more_text = false;
 						filtered[j] = '<b>' + filtered[j] + '</b>';
+						if (Math.floor(j) - 10 > 0){
+							init = Math.floor(j) - 10;
+							if (resume[resume.length - 1] != '...')
+								resume.push('...');
+						} else {
+							init = 0;
+						}
+						if (Math.floor(j) + 10 < filtered.length){
+							more_text = true;
+							end = Math.floor(j) + 10;
+						} else {
+							end = filtered.length;
+						}
+						while(init < end){
+							resume.push(filtered[init]);
+							init++;
+						}
+						if (more_text)
+							resume.push('...');
+					}
 				}
-			callback[element].text = filtered.join(' ');
+			}
+			callback[element].text = resume.join(' ');
 		}
 		if (callback.length == 0)
 			res.render('../views/search', {empty: 'No hay resultados para tu búsqueda'});
