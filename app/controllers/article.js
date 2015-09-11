2fd1c9fb52f2928ad79d35036d43e93cf5c61fc8
Rafael Maldonado <jr.maldonadoh@gmail.com>
2015-09-10 22:57:33 -0500

    absolute path for read the feeds

--- app/controllers/article.js
+++ app/controllers/article.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var path = require('path');
 
 module.exports = function (req, res){
 	var page = req.params.article;
@@ -8,7 +9,7 @@ module.exports = function (req, res){
 		if ((/^\d+$/).test(page[i]))
 			id = page[i];
 	if (id && id > 0 && id < 71){
-		var content = JSON.parse(fs.readFileSync(__dirname + '../../../feeds/art_' + id + '.json', 'utf8'));
+		var content = JSON.parse(fs.readFileSync(path.dirname(require.main.filename) + '/feeds/art_' + id + '.json', 'utf8'));
 		res.render('../views/article', {text: content.text});
 	} else
 		res.render('../views/error');
