286d8807e8f45e66fe3df555faa82d3270ce4207
Rafael Maldonado <jr.maldonadoh@gmail.com>
2015-10-02 17:45:20 -0500

    sitemaps

--- app/expressServer.js
+++ app/expressServer.js
@@ -3,15 +3,21 @@ var swig = require('swig');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var compress = require('compression');
+var sm = require('sitemap');
 var home = require('./controllers/home');
 var article = require('./controllers/article');
 var search = require('./controllers/search');
 var error = require('./controllers/error');
 var logger = require('../utils/logger.js');
+var urlslist = require('../utils/urlslist');
 
 module.exports =  ExpressServer = function(config){
     config = config || {};
-    this.expressServer = express();
+    this.expressServer = express(), sitemap = sm.createSitemap ({
+        hostname: 'http://www.reglamentotransitodf.mx',
+        cacheTime: 600000,
+        urls: urlslist
+    });
     this.expressServer.use(compress());
     this.expressServer.engine('html', swig.renderFile);
     this.expressServer.set('view engine', 'html');
@@ -39,6 +45,15 @@ module.exports =  ExpressServer = function(config){
     this.expressServer.get('/google083d2b55760cc10e.html', function (req, res){
         res.send('google-site-verification: google083d2b55760cc10e.html');
     });
+    this.expressServer.get('/sitemap.xml', function (req, res){
+        sitemap.toXML( function (err, xml) {
+            if (err) {
+                return res.status(500).end();
+            }
+            res.header('Content-Type', 'application/xml');
+            res.send( xml );
+        });
+     });
     this.expressServer.get('*', function (req, res){
         error(req, res);
     });
